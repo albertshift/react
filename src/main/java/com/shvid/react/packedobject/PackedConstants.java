4e5c673043d3caa830b5f6a3807cb3e240dc33ae
Alex Shvid <a@shvid.com>
2013-10-23 12:44:43 -0700

    constants

---
+++ src/main/java/com/shvid/react/packedobject/PackedConstants.java
@@ -0,0 +1,25 @@
+package com.shvid.react.packedobject;
+
+public final class PackedConstants {
+
+	static final int BYTE_SIZE = 1;
+	static final int CHAR_SIZE = 2;
+	static final int SHORT_SIZE = 2;
+	static final int INT_SIZE = 4;
+	static final int LONG_SIZE = 8;
+	static final int FLOAT_SIZE = 4;
+	static final int DOUBLE_SIZE = 8;
+
+	static final int PTR32_SIZE = 4;
+	static final int PTR64_SIZE = 8;
+	static final int CAPACITY_SIZE = 4;
+	
+	static final byte DEFAULT_BYTE = (byte)0;
+	static final char DEFAULT_CHAR = '\000';
+	static final short DEFAULT_SHORT = 0;
+	static final int DEFAULT_INT = 0;
+	static final long DEFAULT_LONG = 0L;
+	static final float DEFAULT_FLOAT = 0.0f;
+	static final double DEFAULT_DOUBLE = 0.0;
+	
+}
