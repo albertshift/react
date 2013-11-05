fdf9b84e120f33d22de348c2c55a0e66c7bde598
Alex Shvid <a@shvid.com>
2013-11-05 10:37:30 -0800

    typesizes added

---
+++ src/main/java/com/reactbase/packedobject/TypeSizes.java
@@ -0,0 +1,26 @@
+package com.reactbase.packedobject;
+
+public enum TypeSizes {
+
+	BYTE(1),
+	CHAR(2),
+	SHORT(2),
+	INT(4),
+	LONG(8),
+	FLOAT(4),
+	DOUBLE(8),
+	PTR32(4),
+	PTR64(8),
+	CAPACITY(4);
+		
+	final private int size;
+	
+	TypeSizes(int sz) {
+		this.size = sz;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+}
